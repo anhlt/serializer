7aa8749d373e6da3b02493852fa241120f5056e6
letuananh <le.tuan.anh@aainc.co.jp>
2015-04-24 20:21:41 +0900

    Add unittest

---
+++ test/SerializerTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: letuananh
+ * Date: 4/24/15
+ * Time: 18:03
+ */
+
+
+
+class TestSerializer extends  Serializer{
+    public function define_fields()
+    {
+        $this->fields['name'] = new CharField(array('allow_blank' => false));
+    }
+}
+
+
+class SerializersTest extends PHPUnit_Framework_TestCase {
+
+
+    public function setUp(){
+        $this->serializer = new TestSerializer();
+    }
+
+    public function test_abc(){
+        $this->assertEquals(0,0);
+    }
+
+    public function test_create(){
+
+    }
+
+}
